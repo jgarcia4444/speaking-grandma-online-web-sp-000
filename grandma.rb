39538a0db8459d88f1924875b33cf51d373ff36d
Jake Garcia <bballwiz4@gmail.com>
2020-02-21 06:55:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(text)
   if text == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif text == text.uppercase
     return "NO, NOT SINCE 1938!"
   else
